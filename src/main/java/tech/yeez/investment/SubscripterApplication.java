b0a42f3bc3bc63502c52e9a19b3b7e3d9f5f71f4
OnlyDragon <OnlyDragon@gmail.com>
2022-06-04 22:23:34 +0900

    'initial-Index'

--- src/main/java/tech/yeez/investment/SubscripterApplication.java
+++ src/main/java/tech/yeez/investment/SubscripterApplication.java
@@ -1,5 +1,35 @@
 package tech.yeez.investment;
 
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.context.ConfigurableApplicationContext;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import tech.yeez.investment.utils.SpringBeanUtil;
+//
+///**
+// * @description: Application
+// * @author: xiangbin
+// * @create: 2022-04-07 16:47
+// **/
+//@Slf4j
+//@SpringBootApplication
+//@MapperScan("tech.yeez.investment.mapper")
+//@EnableFeignClients(basePackages = {"tech.yeez.investment.service.feign"})
+//public class SubscripterApplication {
+//
+//    public static void main(String[] args) {
+//    	log.error("here is SubscripterApplication ");
+//        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(SubscripterApplication.class, args);
+//
+//        SpringBeanUtil.applicationContext = configurableApplicationContext;
+//    } 
+//}
+
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
