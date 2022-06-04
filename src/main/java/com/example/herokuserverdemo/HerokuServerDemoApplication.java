1f551ea7dbbcb9d29ffc881131fcd85acd507428
OnlyDragon <OnlyDragon@gmail.com>
2022-06-04 20:51:17 +0900

    initial-commit

---
+++ src/main/java/com/example/herokuserverdemo/HerokuServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokuserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herokuserverdemo/HerokuServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokuserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
